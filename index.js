a5f2bad2bcca97928e64a9b480a2d4cf4eecdf6a
Max Davidson <davidsonmax@gmail.com>
2016-08-22 19:15:19 +0200

    Switch from node-localstorage to flat-cache

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const googl = require('goo.gl');
 const querystring = require('querystring');
 const vm = require('vm');
-const { JSONStorage } = require('node-localstorage');
+const flatCache = require('flat-cache');
 
 // Not very fast, but reasonably safe, jsonp
 function jsonp(url, params, callbackParam = 'callback') {
@@ -26,15 +26,16 @@ function search({ cache = false, googleKey } = {}) {
 
     if (cache) {
       if (cacheStorage === undefined) {
-        cacheStorage = new JSONStorage('./.cache');
+        cacheStorage = flatCache.load('apartmentor');
       }
       apartments = apartments.filter(apartment => {
-        if (cacheStorage.getItem(apartment.refid) === null) {
-          cacheStorage.setItem(apartment.refid, apartment);
+        if (cacheStorage.getKey(apartment.refid) == null) {
+          cacheStorage.setKey(apartment.refid, apartment);
           return true;
         }
         return false;
       });
+      cacheStorage.save();
     }
 
     if (googleKey && apartments.length > 0) {
